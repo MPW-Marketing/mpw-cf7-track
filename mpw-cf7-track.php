98d52300a6dcfbd4ae9a212fe1e7dceb5fd07cbd
damonmaldonado <damon@mpwmarketing.com>
2016-06-22 12:03:59 -0400

    Create mpw-cf7-track.php

---
+++ mpw-cf7-track.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: MPW CF7 Tracking
+Description: Add Tracking Data to CF7
+Version: 0.1a
+Author: dmm
+Text Domain: mpw-cf7
+*/
+function mpw_cf7_scripts_method() {
+    wp_register_script( 'jscookie', plugins_url( '/js/js.cookie.js' , __FILE__ ), array( 'jquery' ), '2.1', false );
+    wp_enqueue_script('jscookie');
+}
+add_action( 'wp_enqueue_scripts', 'my_scripts_method' );
+function cf7_tracking () {
+$cont = '<span class="cookiediv"></span><script>jQuery( document ).ready( function () {
+var utmzCookie = Cookies.get('__utmz');
+jQuery(".cookiediv").html(utmzCookie);
+});</script>';
+return $cont;
+}
+add_shortcode('cf7_track', 'cf7_tracking');
+
