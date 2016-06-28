6a3fb7e5b9998fecbca543612715a4c167039bed
damonmaldonado <damon@mpwmarketing.com>
2016-06-28 09:22:44 -0400

    Update mpw-cf7-track.php

--- mpw-cf7-track.php
+++ mpw-cf7-track.php
@@ -7,10 +7,6 @@ Author: dmm
 Text Domain: mpw-cf7
 */
 function mpw_cf7_scripts_method() {
-    wp_register_script( 'jscookie', plugins_url( '/js/js.cookie.js' , __FILE__ ), array( 'jquery' ), '2.1', false );
-    wp_enqueue_script( 'jscookie');
-    wp_register_script( 'utm_alternative', plugins_url( '/js/utm_alternative.js' , __FILE__ ), array( 'jquery' ), '1.1', false );
-    wp_enqueue_script( 'utm_alternative');
     wp_register_script( 'sourcebuster', plugins_url( '/js/sourcebuster.min.js' , __FILE__ ), array( 'jquery' ), '0.1', false );
     wp_enqueue_script( 'sourcebuster');
     wp_register_script( 'cf7-track', plugins_url( '/js/cf7-track.js' , __FILE__ ), array( 'jquery', 'sourcebuster' ), '0.1', false );
