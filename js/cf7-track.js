1830afe961d78911dfadabd28182780e9858771e
damonmaldonado <damon@mpwmarketing.com>
2016-06-28 09:24:41 -0400

    Update cf7-track.js

--- js/cf7-track.js
+++ js/cf7-track.js
@@ -3,9 +3,12 @@ function readSourceObject () {
   jQuery(".utmcamp").val(sbjs.get.current.cmp);
   jQuery(".utmmediun").val(sbjs.get.current.mdm);
   jQuery(".utmterm").val(sbjs.get.current.trm);
+    jQuery(".firstutmsource").val(sbjs.get.first.src);
+  jQuery(".firstutmcamp").val(sbjs.get.first.cmp);
+  jQuery(".firstutmmediun").val(sbjs.get.first.mdm);
+  jQuery(".firstutmterm").val(sbjs.get.first.trm);
 }
 
 jQuery( document ).ready( function () {
 sbjs.init();
-readSourceObject();
   });
